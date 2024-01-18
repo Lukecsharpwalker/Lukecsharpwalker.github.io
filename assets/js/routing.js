73a962f808968ee3d1befb17fcbe574f618853db
Luk <Lukasz.jablonski1991@gmail.com>
2024-01-18 20:36:39 +0100

    jquery and samy to js

--- assets/js/routing.js
+++ assets/js/routing.js
@@ -45,21 +45,50 @@
 //     handleRoute();
 // });
 
-$(document).ready(function () {
-    var app = $.sammy(function () {
-        this.get('#/home/', function () {
+// $(document).ready(function () {
+//     var app = $.sammy(function () {
+//         this.get('#/home/', function () {
+//             loadPageContent("assets/pages/home.html");
+//         });
+
+//         this.get('#/about/', function () {
+//             loadPageContent("assets/pages/about.html");
+//         });
+
+//         this.get('#/news/', function () {
+//             loadPageContent("assets/pages/news.html");
+//         });
+
+//         this.get('#/contact/', function () {
+//             loadPageContent("assets/pages/contact.html");
+//         });
+//     });
+
+//     app.run('#/');
+// });
+
+// //  Funkcja do ładowania zawartości strony z pliku HTML
+//     function loadPageContent(page) {
+//         $.get(page, function (data) {
+//             $("main").empty();
+//             $("main").html(data);
+//         });
+//     }
+document.addEventListener("DOMContentLoaded", function() {
+    var app = Sammy(function() {
+        this.get('#/home/', function() {
             loadPageContent("assets/pages/home.html");
         });
 
-        this.get('#/about/', function () {
+        this.get('#/about/', function() {
             loadPageContent("assets/pages/about.html");
         });
 
-        this.get('#/news/', function () {
+        this.get('#/news/', function() {
             loadPageContent("assets/pages/news.html");
         });
 
-        this.get('#/contact/', function () {
+        this.get('#/contact/', function() {
             loadPageContent("assets/pages/contact.html");
         });
     });
@@ -67,10 +96,14 @@ $(document).ready(function () {
     app.run('#/');
 });
 
-//  Funkcja do ładowania zawartości strony z pliku HTML
-    function loadPageContent(page) {
-        $.get(page, function (data) {
-            $("main").empty();
-            $("main").html(data);
-        });
-    }
+function loadPageContent(page) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", page, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var mainElement = document.querySelector("main");
+            mainElement.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
